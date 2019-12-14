bd48ffbf5cfc154a6c4c7b693c52a26a7169a695
Artem <triple8six@gmail.com>
2019-12-14 01:43:53 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			monsters: []
+		};
+	}
+
+	componentWillMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello, world!</h1>
+				{this.state.monsters.map((x) => (
+					<h2 key={x.id}>
+						{x.name} and my I live on {x.address.street}
+					</h2>
+				))}
+			</div>
+		);
+	}
+}
+
+export default App;
+
+// monsters: [
+// 	{
+// 		id: "1",
+// 		name: "Haiixz",
+// 		killmove: "Phaser"
+// 	},
+// 	{
+// 		id: "2",
+// 		name: "Ioax",
+// 		killmove: "Lazer Trap"
+// 	},
+// 	{
+// 		id: "3",
+// 		name: "Qxa",
+// 		killmove: "Ioz zapper"
+// 	}
