91516f1e4f6683f183c018f1d9f18f158ac10567
REROOT <adminroot@zoho.com>
2020-02-08 14:58:08 -0800

    add

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { CardList } from './components/card-list/card-list.component';
-import { SearchBox } from './components/search-box/search-box.component';
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-  }
+		this.state = {
+			monsters: [],
+			searchField: ""
+		};
+	}
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
-  }
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
+	onSearchChange = (event) => {
+		this.setState({ searchField: event.target.value });
+	};
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+	render() {
+		//Destrure this.state, this save entries like "this.state.monsters"
+		const { monsters, searchField } = this.state;
+		//filter based on what is typed in
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
 
-    return (
-      <div className='App'>
-        <h1>Monsters Rolodex</h1>
-        <SearchBox onSearchChange={this.onSearchChange} />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
+		return (
+			<div className="App">
+				<h1>Monsters Rolodex</h1>
+				<SearchBox onSearchChange={this.onSearchChange} />
+				{"pass it down to after filted and take it as prop in card componet"}
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
 }
 
 export default App;
