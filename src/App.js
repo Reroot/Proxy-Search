6e09784fc0d500dfce1f2f3e4bf0056d7d333e68
REROOT <adminroot@zoho.com>
2020-02-09 00:51:59 -0800

    add

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ class App extends Component {
 			monsters: [],
 			searchField: ""
 		};
+		//**** */
+		//if we use arrow fucntions for our method we don't have to do this.
+		//this.onSearchChange = this.onSearchChange.bind(this);
+		//we bind to have the finction refrence across the app
+		//if we write our own method we would have to be carfful how we write it
+		//would need to bind it so it can use the state
 	}
 
 	componentDidMount() {
@@ -20,11 +26,19 @@ class App extends Component {
 			.then((response) => response.json())
 			.then((users) => this.setState({ monsters: users }));
 	}
+	//we could have done this here, but instead it's better to pass in and change the field
+	//with a reusable componet.
+	//Babel help newer js with older js transformations.
 
+	//must state contex for  normal functions methods, otherwise react will not give the scope
+	//But with arrow functions it auto binds to app componet
 	onSearchChange = (event) => {
 		this.setState({ searchField: event.target.value });
 	};
-
+	//Method is recalled/rerendered everytime something is typed, settign a new
+	//arry, and passing it , then it's dynamically updating.
+	//. This is why react is graet we don't have to do extensive calls to show/side elements
+	//react rerenders it for us every call.
 	render() {
 		//Destrure this.state, this save entries like "this.state.monsters"
 		const { monsters, searchField } = this.state;
@@ -35,6 +49,8 @@ class App extends Component {
 		// {
 		// 	("pass it down to after filted and take it as prop in card componet");
 		// }
+		//onSearchChange={e => this.setState({ searchField : e.target.value})} can make a method instead on searchChange
+		//we should write our own method, so we can use it multiple timez
 		return (
 			<div className="App">
 				<h1>Monsters Rolodex</h1>
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './search-box.styles.css';
+import "./search-box.styles.css";
+//functional componets don't have acess to state, since we are not extending componet
+//also they don't have acess to lifecycle methods, just some props
 
-export const SearchBox = props => (
-  <input
-    className='search-box'
-    type='search'
-    placeholder='search monsters'
-    onChange={props.onSearchChange}
-  />
+//we can use props or destructure values
+//const SearchBox = ({ placeholder, handelChange} = this.state)
+// ask yourself what changes when state changes?
+//react has only one way data flow, apps,js is good place to put state,
+//event happens on a componet, tigger an event handeler to let it know
+//upwards to update and then pass it down again updagted from a lower componet
+
+//
+export const SearchBox = (props) => (
+	<input
+		className="search-box"
+		type="search"
+		placeholder="Search monsters"
+		onChange={props.onSearchChange}
+	/>
 );
